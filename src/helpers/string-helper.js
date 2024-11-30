7e5b6453f19e7cef84906c4485affd5bc7120069
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2024-11-30 15:41:36 +0700

    style: format capitalizeWord function for better readability

--- src/helpers/string-helper.js
+++ src/helpers/string-helper.js
@@ -1,5 +1,8 @@
-function capitalizeWord (input) {
-  return input.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+function capitalizeWord(input) {
+  return input
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
-export { capitalizeWord}
+export { capitalizeWord };
