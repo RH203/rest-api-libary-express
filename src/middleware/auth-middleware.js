f06677d6cf369ff11bfb7dfccffa1d2506722b6a
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2024-11-30 13:10:39 +0700

    ``` fix: improve formatting and readability in auth-middleware.js (issue #123) ```

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,22 +1,24 @@
-import {verifyJWT} from "../helpers/jwt-config.js";
+import { verifyJWT } from "../helpers/jwt-config.js";
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({status: 401, message: "Token is required" });
+    return res.status(401).json({ status: 401, message: "Token is required" });
   }
 
   try {
     const decodedToken = verifyJWT(token);
     const timeNow = Date.now() / 1000;
     if (timeNow > decodedToken.exp) {
-      return res.status(401).json({status: 401, message: "Token is expired" });
+      return res.status(401).json({ status: 401, message: "Token is expired" });
     }
     next();
   } catch (error) {
-    return res.status(401).json({status: 401, message: "Invalid or expired token" });
+    return res
+      .status(401)
+      .json({ status: 401, message: "Invalid or expired token" });
   }
-}
+};
 
-export { authMiddleware}
+export { authMiddleware };
