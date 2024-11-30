7f8af8d4302e5a5fa9f4c0e6a87ffe9698b3012b
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2024-11-30 12:27:48 +0700

    feat: Add login and get book endpoints (#123)
    Adds login endpoint and endpoints for getting book
    list and book details.  Requires authentication for
    book endpoints.

--- src/router/public-api.js
+++ src/router/public-api.js
@@ -1,13 +1,17 @@
 import express from "express";
 import publicController from "../controller/public-controller.js";
+import {authMiddleware} from "../middleware/auth-middleware.js";
 
 const publicApi = new express.Router();
 
 // Registrasi
 publicApi.post("/api/user/registrasi", publicController.registrasi);
 // Login
+publicApi.get("/api/user/login", publicController.login);
 // Melihat daftar buku
+publicApi.get("/api/book", authMiddleware, publicController.getBookList)
 // Detail buku
+publicApi.get("/api/book/:id", authMiddleware, publicController.getBookById)
 // Melihat member lain yang aktif
 // meminjam buku
 
