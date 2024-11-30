b47c4034fd58c0233374b2a6d39d9562fa2047bc
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2024-11-30 12:28:10 +0700

    feat: Implement login and book list API (master)
    This commit implements the login API and adds functionality to
    retrieve a list of books and a specific book by ID.
    
    The login API now validates user credentials and returns a JWT
    upon successful authentication.  The book list API retrieves
    all books with their associated genres and publisher
    information.  The book detail API retrieves a specific book
    by its ID, including details like title, author, description,
    ISBN, genres, and publisher.  Error handling has been
    improved to provide more informative error messages.

--- src/service/public-service.js
+++ src/service/public-service.js
@@ -1,24 +1,20 @@
-// Registrasi
-import { ResponseError } from "../error/response-error.js";
-import { prismaClient } from "../app/database.js";
+import {ResponseError} from "../error/response-error.js";
+import {prismaClient} from "../app/database.js";
 import bcrypt from "bcrypt";
-import { Role } from "@prisma/client";
-import { validate } from "../validation/validation.js";
-import { registrationUserValidation } from "../validation/user-validation.js";
-import { logger } from "../app/logger.js";
+import {Role} from "@prisma/client";
+import {validate} from "../validation/validation.js";
+import {loginUserValidation, registrationUserValidation,} from "../validation/user-validation.js";
+import {generateJWT} from "../helpers/jwt-config.js";
 
+// Registrasi
 const registrasi = async (req, res) => {
   const data = validate(registrationUserValidation, req.body);
 
-  if (data.error) {
-    throw new ResponseError(400, data.error);
-  }
-
-  const checkAccountDuplicate = await prismaClient.student.count({
+  const checkAccountDuplicate = await prismaClient.student.findUnique({
     where: { email: data.email },
   });
 
-  if (checkAccountDuplicate >= 1) {
+  if (checkAccountDuplicate) {
     throw new ResponseError(400, "email already exists!");
   }
 
@@ -39,11 +35,109 @@ const registrasi = async (req, res) => {
   });
 };
 
-// Login
-const login = async (req, res) => {};
+// login
+const login = async (req, res) => {
+  const data = validate(loginUserValidation, req.body);
+
+  const account = await prismaClient.student.findFirst({
+    where: { email: data.email },
+    select: { name: true, email: true, password: true, role: true },
+  });
+
+  if (!account) {
+    throw new ResponseError(400, "Email or password is wrong!");
+  }
+
+  const isPasswordSame = await bcrypt.compare(data.password, account.password);
+
+  if (!isPasswordSame) {
+    throw new ResponseError(400, "Email or password is wrong!");
+  }
+
+  return generateJWT(account.name, account.role);
+};
+
 // Melihat daftar buku
+const getBookList = async (req, res) => {
+  const data = await prismaClient.book.findMany({
+    select: {
+      id: true,
+      title: true,
+      author: true,
+      description: true,
+      isbn: true,
+      genres: {
+        select: {
+          genre: {
+            select: {
+              name: true
+            }
+          }
+        }
+      },
+      publisher: {
+        select: {
+          name: true
+        }
+      }
+    }
+  })
+
+  return data.map(book => ({
+    id: book.id,
+    title: book.title,
+    author: book.author,
+    description: book.description,
+    isbn: book.isbn,
+    genres: book.genres.map((genre) => genre.genre.name),
+    publisher: book.publisher?.name || "Unknown",
+  }))
+}
+
 // Detail buku
+const getBookById = async (id) => {
+
+  const data = await prismaClient.book.findFirst({
+    where: { id: id },
+    select: {
+      id: true,
+      title: true,
+      author: true,
+      description: true,
+      isbn: true,
+      genres: {
+        select: {
+          genre: {
+            select: {
+              name: true
+            }
+          }
+        }
+      },
+      publisher: {
+        select: {
+          name: true
+        }
+      }
+    }
+  })
+
+  if (!data) {
+    throw new ResponseError(400, "No book found with id " + req.params.id);
+  }
+
+  return {
+    id: data.id,
+    title: data.title,
+    author: data.author,
+    description: data.description,
+    isbn: data.isbn,
+    genres: data.genres.map((genre) => genre.genre.name),
+    publisher: data.publisher?.name || "Unknown",
+  }
+}
+
 // Melihat member lain yang aktif
 // meminjam buku
 
-export default { registrasi };
+export default { registrasi, login, getBookList, getBookById};
