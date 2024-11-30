db5e2c77e5f10ce29776cfa19075a1d3f0b9a5df
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2024-11-30 13:11:03 +0700

    ```plaintext fix: format import statements and add missing semicolons (issue #123) ```
    (Note: Replace `issue #123` with the actual issue number from your branch name if applicable.)

--- src/router/public-api.js
+++ src/router/public-api.js
@@ -1,6 +1,6 @@
 import express from "express";
 import publicController from "../controller/public-controller.js";
-import {authMiddleware} from "../middleware/auth-middleware.js";
+import { authMiddleware } from "../middleware/auth-middleware.js";
 
 const publicApi = new express.Router();
 
@@ -9,9 +9,9 @@ publicApi.post("/api/user/registrasi", publicController.registrasi);
 // Login
 publicApi.get("/api/user/login", publicController.login);
 // Melihat daftar buku
-publicApi.get("/api/book", authMiddleware, publicController.getBookList)
+publicApi.get("/api/book", authMiddleware, publicController.getBookList);
 // Detail buku
-publicApi.get("/api/book/:id", authMiddleware, publicController.getBookById)
+publicApi.get("/api/book/:id", authMiddleware, publicController.getBookById);
 // Melihat member lain yang aktif
 // meminjam buku
 
