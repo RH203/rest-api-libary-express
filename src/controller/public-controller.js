a0f74ead9f189f481268e08ba2ef9b213e2859b8
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2024-11-30 12:27:57 +0700

    feat: Implement public API endpoints (master)
    This commit implements several public API endpoints:
    
    - Registration: Allows users to register a new account.
      Returns a success message upon successful registration.
    - Login: Enables users to log in with their credentials.
      Returns user data upon successful login.
    - Get Book List: Retrieves a list of available books.
      Returns a list of books.
    - Get Book by ID: Retrieves a specific book by its ID.
      Returns the book details.
    
    The endpoints utilize the `publicService` and include
    proper error handling.  Response format is standardized
    with status codes and informative messages/data.

--- src/controller/public-controller.js
+++ src/controller/public-controller.js
@@ -1,15 +1,68 @@
 import publicService from "../service/public-service.js";
-import { logger } from "../app/logger.js";
+import {logger} from "../app/logger.js";
 
+// Registrasi controller
 const registrasi = async (req, res, next) => {
   try {
-    logger.info(req.body);
     const result = await publicService.registrasi(req);
 
-    res.status(200).json(result);
+    res.status(200).json({
+      status: 200,
+      message: result,
+    });
   } catch (e) {
     next(e);
   }
 };
 
-export default { registrasi };
+// Login controller
+const login = async (req, res, next) => {
+  try {
+    const result = await publicService.login(req);
+    res.status(200).json({
+      status: 200,
+      data: result,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+// Get Book controller
+const getBookList = async (req, res, next) => {
+  try {
+    const result = await publicService.getBookList();
+
+    res.status(200).json({
+      status: 200,
+      data: result,
+    })
+  } catch (e) {
+    next(e);
+  }
+}
+
+// Get book by id
+const getBookById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      res.status(404).json({
+        status: 404,
+        message: 'Not Found',
+      })
+      return;
+    }
+
+    const result = await publicService.getBookById(parseInt(id));
+
+    res.status(200).json({
+      status: 200,
+      data: result,
+    })
+  } catch (e) {
+    next(e);
+  }
+}
+
+export default { registrasi, login, getBookList, getBookById };
